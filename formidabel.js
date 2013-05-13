8999bfd4124df496f9d4b662c27da404b864ed61
Carsten Blüm <carsten@bluem.net>
2013-05-13 20:57:49 +0200

    Fixed erroneous behaviour when used with <textarea> and <input>

--- formidabel.js
+++ formidabel.js
@@ -21,36 +21,35 @@ var Formidabel = new function() {
 	 * Starts tracking of changes in forms.
 	 * Usage: below your form, call Formidabel.init(arg1, arg2, arg3)
      *
-	 * @param {String} formid HTML element ID of the form to be observed.
-	 * @param {String} classname Optional CSS classname that should be added to any
-	 *                           <label> element whose form element has been changed.
-	 * @param {String} labelPrefix Optional string that's used as prefix for (all!)
-	 *                             element names to construct the element IDs (i.e.: if
-	 *                             the element name is "foo" and its ID is "form_foo",
-	 *                             set arg3 to "foo_").
+	 * @param thing       HTML element ID of the form to be observed.
+	 * @param classname   Optional CSS classname that should be added to any
+	 *                    <label> element whose form element has been changed.
+	 * @param labelPrefix Optional string that's used as prefix for (all!)
+	 *                    element names to construct the element IDs (i.e.: if
+	 *                    the element name is "foo" and its ID is "form_foo",
+	 *                    set arg3 to "foo_").
 	 */
-	this.init = function(formid, classname, labelPrefix) {
+	this.init = function(thing, classname, labelPrefix) {
 
-		this.form = $('#' + formid);
+		this.form        = $('#' + thing);
 		this.resetButton = this.form.find('input[type="reset"]').eq(0);
-		this.classname = classname;
-		this.inited = true;
+		this.classname   = classname;
+		this.inited      = true;
 
         if (labelPrefix) {
             this.labelPrefix = labelPrefix;
         }
 
 		this.form.find('input, textarea, select').each(function() {
-			var eventName = Formidabel.eventNameByType($(this).attr('type'));
-			var theName = $(this).attr('name');
+			var eventName = Formidabel.eventNameByType(this);
+			var theName   = $(this).attr('name');
 
-			if (theName && eventName) {
+            if (theName && eventName) {
 				Formidabel.origVals[theName] = Formidabel.elementValue(this);
 				$(this).bind(eventName, function(event) {
 					var formItem = event.target;
-					var currVal = Formidabel.elementValue(formItem);
-					var theName = formItem.name;
-
+					var currVal  = Formidabel.elementValue(formItem);
+					var theName  = formItem.name;
 					var labelElement = $('label[for="' + Formidabel.labelPrefix + theName.replace(/\[\]/, '') + '"]');
 					if (labelElement.length) {
 						labelElement = labelElement[0];
@@ -83,8 +82,9 @@ var Formidabel = new function() {
 
 	/**
 	 * Notifies change of an element to Formidabel
-	 * @param {String} ename Value of form element's "name" attribute
-	 * @param {Object} label Element's <label> (HTML object, not jQuery object)
+     *
+	 * @param ename Value of form element's "name" attribute
+	 * @param label Element's <label> (HTML object, not jQuery object)
 	 */
 	this.markElementAsChanged = function(ename, label) {
 		Formidabel.markFormAsDirty(true);
@@ -96,7 +96,8 @@ var Formidabel = new function() {
 
 	/**
 	 * Returns the value of the given element
-	 * @param {Object} elmnt HTML form element (not jQuery element)
+     *
+	 * @param elmnt     HTML form element (not jQuery element)
 	 * @return {String} In case of radio buttons, returns the value of the
 	 *                  selected radio button. For a checkbox, it returns
 	 *                  either an empty string (not checked) or checkbox's
@@ -123,6 +124,7 @@ var Formidabel = new function() {
 	/**
 	 * Returns whether the contents / state of the given element evaluates
 	 * to true, i.e.: contains value, is checked, has selection etc.
+     *
 	 * @param elmnt     HTML form element (not jQuery element)
 	 * @return {String} In case of radio buttons, returns the value of the selected
 	 *                  radio button. For a checkbox, it returns either an empty string
@@ -159,6 +161,7 @@ var Formidabel = new function() {
 
 	/**
 	 * Returns whether any observed elements were changed
+     *
 	 * @return true, if at least 1 element was changed, false otherwise
 	 * @type {Boolean}
 	 */
@@ -178,6 +181,7 @@ var Formidabel = new function() {
 	 * element or triggers a function that may operate on that element. To
 	 * ease setting the inital state, upon page loading the actions is
 	 * performed as well!
+     *
 	 * @param target Either HTML element ID of the element to be
 	 *               controlled, or the jQuery object for that element.
 	 * @param source Either HTML element ID of the controlling
@@ -198,7 +202,7 @@ var Formidabel = new function() {
 			target = $('#' + target);
 		}
 
-		var eventName = Formidabel.eventNameByType(source.attr('type'));
+		var eventName = Formidabel.eventNameByType(source.get(0));
 
 		if (!eventName) {
 			alert('Unable to bind ' + source.attr('type'));
@@ -251,22 +255,23 @@ var Formidabel = new function() {
 	/**
 	 * Returns the name of the event to be observed based on the element type
      *
-	 * @param {String} type Element type
+	 * @param element Element type
+     *
 	 * @return {String} String such as "click" or "keyup"
 	 */
-	this.eventNameByType = function(type) {
-		switch (type) {
-			case 'textarea':
+	this.eventNameByType = function(element) {
+        switch (element.type.toLowerCase()) {
 			case 'text':
 			case 'password':
+			case 'textarea':
 				return 'keyup';
 			case 'checkbox':
 			case 'radio':
 				return 'click';
 			case 'file':
-			case 'select':
-			case 'select-one':
-			case 'select-multiple':
+            case 'select':
+            case 'select-one':
+            case 'select-multiple':
 				return'change';
 		}
 		return '';
